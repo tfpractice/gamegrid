af13cd2de9d548e66f83d079bdb0c942f844d1d7
theory friction practice <jackson.brebnor@gmail.com>
2016-09-25 20:57:51 -0400

    weird module integrations

--- app/grid.js
+++ app/grid.js
@@ -1,4 +1,31 @@
+const { Graph } = require('functional_graph_theory');
+const { nodes } = Graph;
 const cell = require('./cell');
-const gridData = (columns = 3, rows = 3) => ({ columns, rows });
+const defG = { cNum: 3, rNum: 3, cells: [] };
 
-exports.gridData = gridData;
+let initCells = (cNum = 3, rNum = 3) => {
+	let cells = [];
+	for (var c = cNum - 1; c >= 0; c--) {
+		for (var r = rNum - 1; r >= 0; r--) {
+			cells.push(cell(c, r));
+		}
+	}
+
+	return cells;
+};
+
+const graphCells = ({ edges }) => nodes({ edges });
+
+const cells = ({ cells } = defG) => cells;
+const state = (cNum = 3, rNum = 3) => ({
+	cNum,
+	rNum,
+	cells: initCells(cNum, rNum),
+});
+const makeGrid = ({ cells }) => Graph(...cells);
+// const gridString = ({ cNum, rNum } = defG) =>
+
+exports.state = state;
+exports.makeGrid = makeGrid;
+exports.initCells = initCells;
+exports.cells = cells;
--- spec/grid.spec.js
+++ spec/grid.spec.js
@@ -1,9 +1,32 @@
-describe('cell', function() {
-	beforeEach(function() {});
+describe('grid', function() {
+	beforeAll(function() {
+		console.log('\n.........grid Spec.........');
+	});
 
-	describe('gridData', () => {
-		it('retunrn an object wtih columns and rows attributes', function() {
-			expect(grid.gridData(3, 3)).toBeObject();
+	beforeEach(function() {
+		g66 = grid.state(6, 6);
+		myGrid = grid.makeGrid(g66);
+	});
+
+	describe('state', () => {
+		it('retunrn an object wtih cNum and rNum attributes', function() {
+			expect(grid.state(3, 3)).toBeObject();
+		});
+	});
+	describe('initCells({cNum, rNum})', () => {
+		it('retunrn an object wtih cNum and rNum attributes', function() {
+			expect(grid.initCells(6, 6)).toBeArray();
+		});
+	});
+	describe('cells', () => {
+		it('returns the cells of the grid', function() {
+			expect(grid.cells(g66)).toBeArray();
+		});
+	});
+	describe('makeGrid', () => {
+		it('returns a graph of the cells ', function() {
+			console.log(myGrid.showGraph);
+			expect(grid.makeGrid(g66)).toBeObject();
 		});
 	});
 
