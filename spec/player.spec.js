40db09227a85cb3bd0d3c7050b54e499e8951fc7
theory friction practice <jackson.brebnor@gmail.com>
2016-09-29 14:18:23 -0400

    playerspec cleanup

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -8,51 +8,49 @@ describe('player', function() {
 	beforeEach(function() {
 		john = player('john');
 		jCells = allCells.filter((c, id) => id > 17);
-		n30 = allCells.find(({ column, row }) => column === 5 && row === 0);
-		n31 = allCells.find(({ column, row }) => column === 5 && row === 1);
-		n32 = allCells.find(({ column, row }) => column === 5 && row === 2);
-		P.claimCells(
-			john)(...jCells);
+		P.claimCells(john)(...jCells);
 	});
 
 	describe('when given a name string', () => {
-		it('retuns an object with that name', function() {
+		it('retuns an object with that name', () => {
 			expect(P('john')).toBeObject();
 		});
 	});
-	describe('name(P)', function() {
-		it('retrieves the name attribute', function() {
+	describe('name(P)', () => {
+		it('retrieves the name attribute', () => {
 			expect(P.getName(john)).toBe('john');
 		});
 	});
 
 	describe('score', () => {
-		it('returns the players score', function() {
+		it('returns the players score', () => {
 			expect(P.score(john)).toBe(0);
 		});
 	});
 	describe('incrementScore', () => {
-		it('increases the score by one', function() {
+		it('increases the score by one', () => {
 			P.incrementScore(john);
 			expect(P.score(john)).toBe(1);
 		});
 	});
 	describe('incrementScore', () => {
-		it('increases the score by one', function() {
+		it('increases the score by one', () => {
 			P.decrementScore(john);
 			expect(P.score(john)).toBe(-1);
 		});
 	});
 	describe('getGraph', () => {
-		it('returns the Ps graph object', function() {
+		it('returns the Ps graph object', () => {
 			expect(P.getGraph(john)).toBeObject();
 		});
 	});
 
 	describe('claimCell', () => {
-		it('adds a cell to the Ps graph', function() {
-			let jGraph = P.getGraph(john);
-			P.claimCells(john)(n30, n31, n32);
+		it('adds a cell to the Ps graph', () => {
+			n30 = allCells.find(({ column, row }) =>
+				column === 3 && row === 0);
+
+			P.claimCells(john)(n30);
 			expect(G.edges(P.getGraph(john)).has(n30)).toBeTrue();
 		});
 	});
