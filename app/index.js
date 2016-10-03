aa8854385061f3863ee8f394c1875ff71fdbb9df
theory friction practice <jackson.brebnor@gmail.com>
2016-10-03 17:13:44 -0400

    removes asyncgrid

--- app/index.js
+++ app/index.js
@@ -3,5 +3,4 @@ exports.Connections = require('./connections');
 exports.Grid = require('./grid');
 exports.Game = require('./Game');
 exports.GameGraph = require('./game_graph');
-// exports.AsyncGrid = require('./async_grid');
 exports.Player = require('./player');
