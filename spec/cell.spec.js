c15f093d20290933af9a1b32a71fb34422ff0c14
theory friction practice <jackson.brebnor@gmail.com>
2016-10-11 10:26:30 -0400

    spec separtor

--- spec/cell.spec.js
+++ spec/cell.spec.js
@@ -16,9 +16,16 @@ fdescribe('cell', function() {
 		it('is a function', () => {
 			expect(Cell.spawn).toBeFunction();
 		});
+		describe('when given a row and column indices', () => {
+			it('returns an Object with row and column properties', () => {
+				expect(Cell.spawn(2, 3)).toBeObject();
+				expect(c01.column).toBe(0);
+				expect(c01.row).toBe(1);
+			});
+		});
 	});
 
-	describe('operators', () => {
+	describe('queries', () => {
 		describe('row(cell)', () => {
 			it('retuns the row index', () => {
 				expect(Cell.row(c01)).toBe(01);
@@ -30,13 +37,27 @@ fdescribe('cell', function() {
 			});
 		});
 		describe('player', () => {
-			it('returns the player attribute', function() {
+			it('returns the player attribute', () => {
 				expect(Cell.player(c00)).toBeNull();
 			});
 		});
 		describe('cellString(cell)', () => {
 			it('returns a string representation', () => {
 				expect(Cell.cellString(c00)).toBeString();
+			});
+		});
+	});
+	describe('operators', () => {
+		describe('claim', () => {
+			it('sets the objects player attribute', () => {
+				Cell.claim(c00)({ name: 'jack' });
+				expect(Cell.player(c00)).not.toBeNull();
+			});
+		});
+		describe('unclaim', () => {
+			it('sets the objects player attribute', () => {
+				Cell.unclaim(c00);
+				expect(Cell.player(c00)).toBeNull();
 			});
 		});
 		describe('colDiff', () => {
@@ -49,6 +70,26 @@ fdescribe('cell', function() {
 				expect(Math.abs(Cell.rowDiff(c00)(c01))).toBe(1);
 			});
 		});
+
+		describe('tangent', () => {
+			it('returns coldiff/rowDiff', () => {
+				expect(Math.abs(Cell.tangent(c00)(c01))).toBe(Infinity);
+			});
+		});
+
+		describe('angleBetween', () => {
+			it('returns the angle between two nodes in radians', () => {
+				expect(Math.abs(Cell.angleBetween(c00)(c01))).toBe(Math.PI / 2);
+			});
+		});
+		describe('samePVector', () => {
+			it('checks if the anglebetween two nodes is PI/4', () => {
+				expect(Cell.samePVector(c22)(c33)).toBeTrue();
+			});
+		});
+	});
+
+	describe('comparitors', () => {
 		describe('cAdj', () => {
 			it('checks if the colDiff is < 2', () => {
 				expect(Cell.cAdj(c00)(c01)).toBeTrue();
@@ -59,24 +100,9 @@ fdescribe('cell', function() {
 				expect(Cell.rAdj(c00)(c01)).toBeTrue();
 			});
 		});
-		describe('tangent', function() {
-			it('returns coldiff/rowDiff', function() {
-				expect(Math.abs(Cell.tangent(c00)(c01))).toBe(Infinity);
-			});
-		});
 
-		describe('angleBetween', () => {
-			it('returns the angle between two nodes in radians', function() {
-				expect(Math.abs(Cell.angleBetween(c00)(c01))).toBe(Math.PI / 2);
-			});
-		});
-		describe('samePVector', () => {
-			it('checks if the anglebetween two nodes is PI/4', function() {
-				expect(Cell.samePVector(c22)(c33)).toBeTrue();
-			});
-		});
 		describe('sameNVector', () => {
-			it('checks if the anglebetween two nodes is PI/4', function() {
+			it('checks if the anglebetween two nodes is PI/4', () => {
 				expect(Cell.sameNVector(c31)(c22)).toBeTrue();
 			});
 		});
@@ -106,31 +132,11 @@ fdescribe('cell', function() {
 				expect(Cell.x_isEquivalent(c00)(c01)).toBeTrue();
 			});
 		});
-		describe('claim', () => {
-			it('sets the objects player attribute', function() {
-				Cell.claim(c00)({ name: 'jack' });
-				expect(Cell.player(c00)).not.toBeNull();
-			});
-		});
-		describe('unclaim', () => {
-			it('sets the objects player attribute', function() {
-				Cell.unclaim(c00);
-				expect(Cell.player(c00)).toBeNull();
-			});
-		});
 
-		describe('samePlayer', function() {
-			it('checks if the player attributes are identical', function() {
+		describe('samePlayer', () => {
+			it('checks if the player attributes are identical', () => {
 				expect(Cell.samePlayer(c00)(c01)).toBeTrue();
 			});
-		});
-	});
-
-	describe('when given a row and column indices', () => {
-		it('returns an Object with row and column properties', () => {
-			expect(Cell.spawn(2, 3)).toBeObject();
-			expect(c01.column).toBe(0);
-			expect(c01.row).toBe(1);
 		});
 	});
 });
