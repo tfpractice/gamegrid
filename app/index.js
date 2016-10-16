92e25cb945d190ad42eb21508c24a71ea20b4bb1
theory friction practice <jackson.brebnor@gmail.com>
2016-10-15 22:34:31 -0400

    removes plyear and graph from index

--- app/index.js
+++ app/index.js
@@ -2,7 +2,4 @@ exports.Cell = require('./cell');
 exports.Connections = require('./connections');
 exports.Traversals = require('./traversals');
 exports.Grid = require('./grid');
-// expo
-// exports.Game = require('./Game');
 exports.GameGraph = require('./game_graph');
-// exports.Player = require('./player');
