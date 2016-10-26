086c3f8e27c8ad66f8d31d593b71577c1a5c7668
theory friction practice <jackson.brebnor@gmail.com>
2016-10-26 19:39:03 -0400

    exports Utils from functional_graph_theory, reducing future dependencies

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+const { Utils } = require('functional_graph_theory');
+exports.Utils = Utils;
+
 exports.Cell = require('./cell');
 exports.Connections = require('./connections');
 exports.Traversals = require('./traversals');
