894e3853887c40563377854dc2aa7f8707631424
theory friction practice <jackson.brebnor@gmail.com>
2017-01-05 15:49:15 -0500

    cell spec 100%

--- spec/cell.spec.js
+++ spec/cell.spec.js
@@ -1,27 +1,11 @@
-// import * as Cell from 'src/cell';
-require('jasmine-expect');
-const Cell = require('../src/cell');
+import 'jasmine-expect';
+import * as Cell from 'src/cell';
+
 const c00 = Cell.spawn(0, 0);
 const c01 = Cell.spawn(0, 1);
 const c22 = Cell.spawn(2, 2);
 const c31 = Cell.spawn(3, 1);
 const c33 = Cell.spawn(3, 3);
-
-//
-// describe('Cell', () => {
-//   beforeAll(() => {
-//     console.log('\n.........Cell Spec.........');
-//
-//     // ({ Cell } = app);
-//   });
-
-  // beforeEach(() => {
-    // const c00 = Cell.spawn(0, 0);
-    // const c01 = Cell.spawn(0, 1);
-    // const c22 = Cell.spawn(2, 2);
-    // const c31 = Cell.spawn(3, 1);
-    // const c33 = Cell.spawn(3, 3);
-  // });
 
 describe('spawn', () => {
   it('is a function', () => {
@@ -36,96 +20,98 @@ describe('spawn', () => {
   });
 });
 
-  // describe('queries', () => {
-    // describe('row(cell)', () => {
-      // it('retuns the row index', () => {
-        // expect(Cell.row(c01)).toBe('01');
-      // });
-    // });
-    // describe('column(cell)', () => {
-      // it('retuns the column index', () => {
-        // expect(Cell.column(c00)).toBe(0);
-      // });
-    // });
-//
-    // describe('cellString(cell)', () => {
-      // it('returns a string representation', () => {
-        // expect(Cell.cellString(c00)).toBeString();
-      // });
-    // });
-  // });
-  // describe('operators', () => {
-    // describe('colDiff', () => {
-      // it('returns the difference in column attributes', () => {
-        // expect(Cell.colDiff(c00)(c01)).toBe(0);
-      // });
-    // });
-    // describe('rowDiff', () => {
-      // it('returns the difference in column attributes', () => {
-        // expect(Math.abs(Cell.rowDiff(c00)(c01))).toBe(1);
-      // });
-    // });
-//
-    // describe('tangent', () => {
-      // it('returns coldiff/rowDiff', () => {
-        // expect(Math.abs(Cell.tangent(c00)(c01))).toBe(Infinity);
-      // });
-    // });
-//
-    // describe('angleBetween', () => {
-      // it('returns the angle between two nodes in radians', () => {
-        // expect(Math.abs(Cell.angleBetween(c00)(c01))).toBe(Math.PI / 2);
-      // });
-    // });
-    // describe('samePVector', () => {
-      // it('checks if the anglebetween two nodes is PI/4', () => {
-        // expect(Cell.samePVector(c22)(c33)).toBeTrue();
-      // });
-    // });
-  // });
+describe('queries', () => {
+  describe('row(cell)', () => {
+    it('retuns the row index', () => {
+      expect(Cell.row(c01)).toBe(1);
+    });
+  });
+  describe('column(cell)', () => {
+    it('retuns the column index', () => {
+      expect(Cell.column(c00)).toBe(0);
+    });
+  });
 
-  // describe('comparitors', () => {
-    // describe('cAdj', () => {
-      // it('checks if the colDiff is < 2', () => {
-        // expect(Cell.cAdj(c00)(c01)).toBeTrue();
-      // });
-    // });
-    // describe('rAdj', () => {
-      // it('checks if the colDiff is < 2', () => {
-        // expect(Cell.rAdj(c00)(c01)).toBeTrue();
-      // });
-    // });
-//
-    // describe('sameNVector', () => {
-      // it('checks if the anglebetween two nodes is PI/4', () => {
-        // expect(Cell.sameNVector(c31)(c22)).toBeTrue();
-      // });
-    // });
-//
-    // describe('sameCol', () => {
-      // it('checks cell columns a difference of 0', () => {
-        // expect(Cell.sameCol(c00)(c01)).toBeTrue();
-      // });
-    // });
-    // describe('sameRow', () => {
-      // it('checks cell rows a difference of 0', () => {
-        // expect(Cell.sameRow(c00)(c01)).toBeFalse();
-      // });
-    // });
-    // describe('isNeighbor', () => {
-      // it('checks for colDiff and rowDiff <2', () => {
-        // expect(Cell.isNeighbor(c00)(c01)).toBeTrue();
-      // });
-    // });
-    // describe('isEquivalent', () => {
-      // it('checks if cells share both row and column', () => {
-        // expect(Cell.isEquivalent(c00)(c00)).toBeTrue();
-      // });
-    // });
-    // describe('x_isEquivalent', () => {
-      // it('checks if cells share both row and column', () => {
-        // expect(Cell.x_isEquivalent(c00)(c01)).toBeTrue();
-      // });
-    // });
-  // });
-// });
+  describe('cellString(cell)', () => {
+    it('returns a string representation', () => {
+      expect(Cell.cellString(c00)).toBeString();
+    });
+  });
+});
+
+describe('operators', () => {
+  describe('colDiff', () => {
+    it('returns the difference in column attributes', () => {
+      expect(Cell.colDiff(c00)(c01)).toBe(0);
+    });
+  });
+  describe('rowDiff', () => {
+    it('returns the difference in column attributes', () => {
+      expect(Math.abs(Cell.rowDiff(c00)(c01))).toBe(1);
+    });
+  });
+
+  describe('tangent', () => {
+    it('returns coldiff/rowDiff', () => {
+      expect(Math.abs(Cell.tangent(c00)(c01))).toBe(Infinity);
+    });
+  });
+
+  describe('angleBetween', () => {
+    it('returns the angle between two nodes in radians', () => {
+      expect(Math.abs(Cell.angleBetween(c00)(c01))).toBe(Math.PI / 2);
+    });
+  });
+  describe('samePVector', () => {
+    it('checks if the anglebetween two nodes is PI/4', () => {
+      expect(Cell.samePVector(c22)(c33)).toBeTrue();
+    });
+  });
+});
+
+describe('comparitors', () => {
+  describe('cAdj', () => {
+    it('checks if the colDiff is < 2', () => {
+      expect(Cell.cAdj(c00)(c01)).toBeTrue();
+    });
+  });
+  describe('rAdj', () => {
+    it('checks if the colDiff is < 2', () => {
+      expect(Cell.rAdj(c00)(c01)).toBeTrue();
+    });
+  });
+
+  describe('sameNVector', () => {
+    it('checks if the anglebetween two nodes is PI/4', () => {
+      expect(Cell.sameNVector(c31)(c22)).toBeTrue();
+    });
+  });
+
+  describe('sameCol', () => {
+    it('checks cell columns a difference of 0', () => {
+      expect(Cell.sameCol(c00)(c01)).toBeTrue();
+    });
+  });
+  describe('sameRow', () => {
+    it('checks cell rows a difference of 0', () => {
+      expect(Cell.sameRow(c00)(c01)).toBeFalse();
+    });
+  });
+  describe('isNeighbor', () => {
+    it('checks for colDiff and rowDiff <2', () => {
+      expect(Cell.isNeighbor(c00)(c01)).toBeTrue();
+    });
+  });
+  describe('isEquivalent', () => {
+    it('checks if cells share both row and column', () => {
+      expect(Cell.isEquivalent(c00)(c00)).toBeTrue();
+    });
+  });
+  describe('xEquivalent', () => {
+    it('checks if cells share both row and column', () => {
+      expect(Cell.xEquivalent(c00)(c01)).toBeTrue();
+    });
+  });
+});
+
+// });/
--- src/cell.js
+++ src/cell.js
@@ -1,47 +1,27 @@
- const { atan, abs, PI } = Math;
+const { atan, abs, PI } = Math;
 
- export const spawn = (column = null, row = null) =>
-  ({ column, row, toString: () => cellString({ column, row }) });
+export const spawn = (column = null, row = null) =>
+ ({ column, row, toString: () => cellString({ column, row }) });
 
- export const column = ({ column }) => column;
- export const row = ({ row }) => row;
- export const cellString = ({ column, row }) => `{ cell::${column}_${row} }`;
+export const column = ({ column }) => column;
+export const row = ({ row }) => row;
+export const cellString = ({ column, row }) => `{ cell::${column}_${row} }`;
 
- export const colDiff = ({ column: c0 }) => ({ column: c1 }) => (c0 - c1);
- export const rowDiff = ({ row: r0 }) => ({ row: r1 }) => (r0 - r1);
- export const tangent = n0 => n1 => (rowDiff(n0)(n1)) / (colDiff(n0)(n1));
- export const angleBetween = n0 => n1 => ((atan(tangent(n0)(n1)) % PI) + PI) % PI;
+export const colDiff = ({ column: c0 }) => ({ column: c1 }) => (c0 - c1);
+export const rowDiff = ({ row: r0 }) => ({ row: r1 }) => (r0 - r1);
+export const tangent = n0 => n1 => (rowDiff(n0)(n1)) / (colDiff(n0)(n1));
+export const angleBetween = n0 => n1 => ((atan(tangent(n0)(n1)) % PI) + PI) % PI;
 
- export const sameCol = n0 => n1 => abs(colDiff(n0)(n1)) === 0;
- export const sameRow = n0 => n1 => abs(rowDiff(n0)(n1)) === 0;
- export const samePVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.25;
- export const sameNVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.75;
+export const sameCol = n0 => n1 => abs(colDiff(n0)(n1)) === 0;
+export const sameRow = n0 => n1 => abs(rowDiff(n0)(n1)) === 0;
+export const samePVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.25;
+export const sameNVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.75;
 
- export const cAdj = n0 => n1 => abs(colDiff(n0)(n1)) < 2;
- export const rAdj = n0 => n1 => abs(rowDiff(n0)(n1)) < 2;
+export const cAdj = n0 => n1 => abs(colDiff(n0)(n1)) < 2;
+export const rAdj = n0 => n1 => abs(rowDiff(n0)(n1)) < 2;
 
- export const isEquivalent = c0 => c1 => sameCol(c0)(c1) && sameRow(c0)(c1);
- export const x_isEquivalent = src => alt => !isEquivalent(src)(alt);
+export const isEquivalent = c0 => c1 => sameCol(c0)(c1) && sameRow(c0)(c1);
+export const xEquivalent = src => alt => !isEquivalent(src)(alt);
 
- export const isNeighbor = n0 => n1 =>
-  x_isEquivalent(n0)(n1) && cAdj(n0)(n1) && rAdj(n0)(n1);
-
- // module.exports = {
- //  spawn,
- //  column,
- //  row,
- //  cellString,
- //  colDiff,
- //  rowDiff,
- //  tangent,
- //  angleBetween,
- //  samePVector,
- //  sameNVector,
- //  cAdj,
- //  rAdj,
- //  isNeighbor,
- //  isEquivalent,
- //  x_isEquivalent,
- //  sameCol,
- //  sameRow,
- // };
+export const isNeighbor = n0 => n1 =>
+ xEquivalent(n0)(n1) && cAdj(n0)(n1) && rAdj(n0)(n1);
