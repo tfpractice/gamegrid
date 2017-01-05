720c78d5fa24255ae05062f809361d6bb94d387d
theory friction practice <jackson.brebnor@gmail.com>
2017-01-05 15:41:17 -0500

    app-> src

--- spec/traversals.spec.js
+++ spec/traversals.spec.js
@@ -1,59 +1,59 @@
-describe('Traversals', function() {
-	beforeAll(function() {
-		console.log('\n.........Traversals Spec.........');
-		({ Grid, Traversals } = app);
-	});
+xdescribe('Traversals', () => {
+  beforeAll(() => {
+    console.log('\n.........Traversals Spec.........');
+    ({ Grid, Traversals } = app);
+  });
 
-	beforeEach(function() {
-		myGraph = Grid.spawn();
-		Grid.addNodes(myGraph)(...allCells);
-	});
+  beforeEach(() => {
+    myGraph = Grid.spawn();
+    Grid.addNodes(myGraph)(...allCells);
+  });
 
-	describe('graph representations', () => {
-		describe('omniGraph', () => {
-			it('returns a new Graph with all neighbors joined', () => {
-				expect(Traversals.omniGraph(myGraph) instanceof Map).toBeTrue();
-			});
-		});
-		describe('colGraph', () => {
-			it('returns a new Graph with only columns joined', () => {
-				expect(Traversals.colGraph(myGraph) instanceof Map).toBeTrue();
-			});
-		});
-		describe('rowGraph', () => {
-			it('returns a new Graph with only columns joined', () => {
-				expect(Traversals.rowGraph(myGraph) instanceof Map).toBeTrue();
-			});
-		});
-		describe('posGraph', () => {
-			it('returns a new Graph with only columns joined', () => {
-				expect(Traversals.posGraph(myGraph) instanceof Map).toBeTrue();
-			});
-		});
-		describe('negGraph', () => {
-			it('returns a new Graph with only columns joined', () => {
-				expect(Traversals.negGraph(myGraph) instanceof Map).toBeTrue();
-			});
-		});
-		describe('colComponents', () => {
-			it('returns the connected components in a column graph', () => {
-				expect(Traversals.colComponents(myGraph) instanceof Set).toBeTrue();
-			});
-		});
-		describe('rowComponents', () => {
-			it('returns the connected components in a column graph', () => {
-				expect(Traversals.rowComponents(myGraph) instanceof Set).toBeTrue();
-			});
-		});
-		describe('posComponents', () => {
-			it('returns the connected components in a column graph', () => {
-				expect(Traversals.posComponents(myGraph) instanceof Set).toBeTrue();
-			});
-		});
-		describe('negComponents', () => {
-			it('returns the connected components in a column graph', () => {
-				expect(Traversals.negComponents(myGraph) instanceof Set).toBeTrue();
-			});
-		});
-	});
+  describe('graph representations', () => {
+    describe('omniGraph', () => {
+      it('returns a new Graph with all neighbors joined', () => {
+        expect(Traversals.omniGraph(myGraph) instanceof Map).toBeTrue();
+      });
+    });
+    describe('colGraph', () => {
+      it('returns a new Graph with only columns joined', () => {
+        expect(Traversals.colGraph(myGraph) instanceof Map).toBeTrue();
+      });
+    });
+    describe('rowGraph', () => {
+      it('returns a new Graph with only columns joined', () => {
+        expect(Traversals.rowGraph(myGraph) instanceof Map).toBeTrue();
+      });
+    });
+    describe('posGraph', () => {
+      it('returns a new Graph with only columns joined', () => {
+        expect(Traversals.posGraph(myGraph) instanceof Map).toBeTrue();
+      });
+    });
+    describe('negGraph', () => {
+      it('returns a new Graph with only columns joined', () => {
+        expect(Traversals.negGraph(myGraph) instanceof Map).toBeTrue();
+      });
+    });
+    describe('colComponents', () => {
+      it('returns the connected components in a column graph', () => {
+        expect(Traversals.colComponents(myGraph) instanceof Set).toBeTrue();
+      });
+    });
+    describe('rowComponents', () => {
+      it('returns the connected components in a column graph', () => {
+        expect(Traversals.rowComponents(myGraph) instanceof Set).toBeTrue();
+      });
+    });
+    describe('posComponents', () => {
+      it('returns the connected components in a column graph', () => {
+        expect(Traversals.posComponents(myGraph) instanceof Set).toBeTrue();
+      });
+    });
+    describe('negComponents', () => {
+      it('returns the connected components in a column graph', () => {
+        expect(Traversals.negComponents(myGraph) instanceof Set).toBeTrue();
+      });
+    });
+  });
 });
