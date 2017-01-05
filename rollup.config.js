4d3b09e4ba640b91d785ebdbe7d25737c3c998b8
theory friction practice <jackson.brebnor@gmail.com>
2017-01-05 15:21:52 -0500

    adds files for config

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import replace from 'rollup-plugin-replace';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import progress from 'rollup-plugin-progress';
+import visualizer from 'rollup-plugin-visualizer';
+
+export default {
+  entry: 'index.js',
+  format: 'umd',
+  dest: 'dist/bundle.umd.js',
+  moduleId: 'game-grid',
+  moduleName: 'game-grid',
+  sourceMap: true,
+  external: [ 'turmeric', 'functional_graph_theory', ],
+  plugins: [
+    progress({ clearLine: false, }),
+    filesize(),
+    nodeResolve(),
+    commonjs(),
+    babel(),
+    visualizer({ filename: 'stats.html', }),
+    replace({ ENV: JSON.stringify(process.env.NODE_ENV || 'development'), }),
+    (process.env.NODE_ENV === 'production' && uglify({ beautify: true, })),
+  ],
+};
---
+++ rollup.es6.config.js
@@ -0,0 +1,23 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import progress from 'rollup-plugin-progress';
+import visualizer from 'rollup-plugin-visualizer';
+
+export default {
+    entry: 'index.js',
+    dest: 'dist/bundle.es6.js',
+    format: 'es',
+    moduleId: 'game-grid',
+    moduleName: 'game-grid',
+    external: [ 'turmeric', 'functional_graph_theory', ],
+    sourceMap: true,
+    plugins: [
+      progress({ clearLine: false, }),
+      filesize(),
+      nodeResolve(),
+      commonjs(),
+      visualizer({ filename: 'stats.es6.html', }),
+      babel({}), ],
+};
