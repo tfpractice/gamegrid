c4a7c24408d6fb9358ba548f4b4fb0b302dcfbb9
theory friction practice <jackson.brebnor@gmail.com>
2017-01-07 23:37:29 -0500

    remove comments from index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// const { Utils } = require('graph-curry');
 
 import * as Node from './node';
 
@@ -7,10 +6,4 @@ import * as Connections from './connections';
 import * as Traversals from './traversals';
 import * as Grid from './grid';
 
-// exports.Utils = Utils;
-
-// exports.Node = require('./node');
-// exports.Connections = require('./connections');
-// exports.Traversals = require('./traversals');
-// exports.Grid = require('./grid');
 export { Connections, Grid, Node, Traversals, };
