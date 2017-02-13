7d08643b3c71c58522936601e2fb13968a1181c7
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 16:01:29 -0500

    join docs

--- src/join.js
+++ src/join.js
@@ -3,10 +3,24 @@ import { allAdj, colAdj, negAdj, posAdj, rowAdj, } from './adj';
 
 const { addEdges, nodes, } = Graph;
 
+// **joinAdj** `::  (Map<edge>, node)  -> Map<edge>`
+// returns a copy of a grid with edges joining a nodes and all its neighbors
 export const joinAdj = (g, src) => addEdges(g)(src, 0)(...allAdj(g)(src));
+
+// **joinCols** `::  (Map<edge>, node)  -> Map<edge>`
+// returns a copy of a grid with edges joining a nodes and all its column neighbors
 export const joinCols = (g, src) => addEdges(g)(src, 0)(...colAdj(g)(src));
+
+// **joinRows** `::  (Map<edge>, node)  -> Map<edge>`
+// returns a copy of a grid with edges joining a nodes and all its row neighbors
 export const joinRows = (g, src) => addEdges(g)(src, 0)(...rowAdj(g)(src));
+
+// **joinPVectors** `::  (Map<edge>, node)  -> Map<edge>`
+// returns a copy of a grid with edges joining a nodes and all its positive neighbors
 export const joinPVectors = (g, src) => addEdges(g)(src, 0)(...posAdj(g)(src));
+
+// **joinNVectors** `::  (Map<edge>, node)  -> Map<edge>`
+// returns a copy of a grid with edges joining a nodes and all its negative neighbors
 export const joinNVectors = (g, src) => addEdges(g)(src, 0)(...negAdj(g)(src));
 
 export const joinGrid = grid => nodes(grid).reduce(joinAdj, grid);
