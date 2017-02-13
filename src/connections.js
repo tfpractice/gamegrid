bcd1c790909d0fb67aca9e6e27d58d7e3724c2df
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 15:16:09 -0500

    nullifies trav and connect modules

--- src/connections.js
+++ src/connections.js
@@ -1,32 +1,32 @@
-import { Graph, } from 'graph-curry';
-import { isNeighbor, sameCol, sameNVector, samePVector, sameRow, } from './node';
-const { addEdges, nodes, } = Graph;
-
-export const adjNodes = g => src => nodes(g).filter(isNeighbor(src));
-
-export const rowAdj = g => src => adjNodes(g)(src).filter(sameRow(src));
-export const colAdj = g => src => adjNodes(g)(src).filter(sameCol(src));
-export const posAdj = g => src => adjNodes(g)(src).filter(samePVector(src));
-export const negAdj = g => src => adjNodes(g)(src).filter(sameNVector(src));
-export const allAdj = g => src => adjNodes(g)(src).filter(isNeighbor(src));
-
-export const joinAdjBin = (grid, src) =>
-  addEdges(grid)(src, 0)(...adjNodes(grid)(src));
-
-export const joinColsBin = (grid, src) =>
-  addEdges(grid)(src, 0)(...colAdj(grid)(src));
-
-export const joinRowsBin = (grid, src) =>
-  addEdges(grid)(src, 0)(...rowAdj(grid)(src));
-
-export const joinPVectorsBin = (grid, src) =>
-  addEdges(grid)(src, 0)(...posAdj(grid)(src));
-
-export const joinNVectorsBin = (grid, src) =>
-  addEdges(grid)(src, 0)(...negAdj(grid)(src));
-
-export const joinAdj = grid => nodes(grid).reduce(joinAdjBin, grid);
-export const joinCols = grid => nodes(grid).reduce(joinColsBin, grid);
-export const joinRows = grid => nodes(grid).reduce(joinRowsBin, grid);
-export const joinPVectors = grid => nodes(grid).reduce(joinPVectorsBin, grid);
-export const joinNVectors = grid => nodes(grid).reduce(joinNVectorsBin, grid);
+// import { Graph, } from 'graph-curry';
+// import { isNeighbor, sameCol, sameNVector, samePVector, sameRow, } from './node';
+// const { addEdges, nodes, } = Graph;
+//
+// export const adjNodes = g => src => nodes(g).filter(isNeighbor(src));
+//
+// export const rowAdj = g => src => adjNodes(g)(src).filter(sameRow(src));
+// export const colAdj = g => src => adjNodes(g)(src).filter(sameCol(src));
+// export const posAdj = g => src => adjNodes(g)(src).filter(samePVector(src));
+// export const negAdj = g => src => adjNodes(g)(src).filter(sameNVector(src));
+// export const allAdj = g => src => adjNodes(g)(src).filter(isNeighbor(src));
+//
+// export const joinAdjBin = (grid, src) =>
+//   addEdges(grid)(src, 0)(...adjNodes(grid)(src));
+//
+// export const joinColsBin = (grid, src) =>
+//   addEdges(grid)(src, 0)(...colAdj(grid)(src));
+//
+// export const joinRowsBin = (grid, src) =>
+//   addEdges(grid)(src, 0)(...rowAdj(grid)(src));
+//
+// export const joinPVectorsBin = (grid, src) =>
+//   addEdges(grid)(src, 0)(...posAdj(grid)(src));
+//
+// export const joinNVectorsBin = (grid, src) =>
+//   addEdges(grid)(src, 0)(...negAdj(grid)(src));
+//
+// export const joinAdj = grid => nodes(grid).reduce(joinAdjBin, grid);
+// export const joinCols = grid => nodes(grid).reduce(joinColsBin, grid);
+// export const joinRows = grid => nodes(grid).reduce(joinRowsBin, grid);
+// export const joinPVectors = grid => nodes(grid).reduce(joinPVectorsBin, grid);
+// export const joinNVectors = grid => nodes(grid).reduce(joinNVectorsBin, grid);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import * as Components from './components';
 import * as Grid from './grid';
 import * as Join from './Join';
 import * as Node from './node';
-export * from './connections';
-export * from './traversals';
-export * from './grid';
-export { default as grid, } from './grid';
-export { default as node, } from './node';
+
+// export * from './connections';
+// export * from './traversals';
+// export * from './grid';
+// export { default as grid, } from './grid';
+// export { default as node, } from './node';
 export { Adj, Components, Grid, Join, Node, };
