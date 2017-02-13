09a3788a49032427331e2588b371a7b2a60e20c1
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 15:38:11 -0500

    docs adj

--- src/adj.js
+++ src/adj.js
@@ -2,8 +2,22 @@ import { Graph, } from 'graph-curry';
 import { isNeighbor, sameCol, sameNVector, samePVector, sameRow, } from './node';
 const { nodes, } = Graph;
 
+// **allAdj** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a graph with edges connecting all nodes
 export const allAdj = g => src => nodes(g).filter(isNeighbor(src));
+
+// **rowAdj** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a graph with edges connecting all rows
 export const rowAdj = g => src => allAdj(g)(src).filter(sameRow(src));
+
+// **colAdj** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a graph with edges connecting all columns
 export const colAdj = g => src => allAdj(g)(src).filter(sameCol(src));
+
+// **posAdj** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a graph with edges connecting all positive diagonals
 export const posAdj = g => src => allAdj(g)(src).filter(samePVector(src));
+
+// **negAdj** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a graph with edges connecting all negative diagonal
 export const negAdj = g => src => allAdj(g)(src).filter(sameNVector(src));
