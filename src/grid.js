183bb7a1e1af86c146f2cf4bafdb59316fc4712d
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 15:58:35 -0500

    docs grid and node

--- src/grid.js
+++ src/grid.js
@@ -4,6 +4,8 @@ import { column as getCol, row as getRow, isEquivalent, node, sameCol,
 
 const { fromElements, nodes, } = Graph;
 
+// **genNodes** `::  (Number, Number) -> [Node]`
+// returns an array of nodes the specified number of columns and rows
 export const genNodes = (cols = 0, rows = 0) => {
   const nArr = [];
   
@@ -16,23 +18,43 @@ export const genNodes = (cols = 0, rows = 0) => {
   return nArr;
 };
 
+// **cIDs** `::  Map<edge> -> Set<Number>`
+// returns a Set of a grid's columns
 export const cIDs = grid => new Set(nodes(grid).map(getCol));
+
+// **rIDs** `::  Map<edge> -> Set<Number>`
+// returns a Set of a grid's rows
 export const rIDs = grid => new Set(nodes(grid).map(getRow));
 
+// **grid** `::  (Number, Number) -> Map<edge>`
+// returns a Map of edges with the specified number of columns and rows
 export const grid = (c = 0, r = 0) => fromElements(...genNodes(c, r));
+
+// **copy** `::  Map<edge> ->  node  -> Map<edge>`
+// returns a copy of a grid
 export const copy = grid => fromElements(...nodes(grid));
 
+// **nodesByColumn** `::  Map<edge> ->  Number  -> [Node]`
+// returns an array of nodes  with the specified column id
 export const nodesByColumn = grid => (column = 0) =>
   nodes(grid).filter(sameCol({ column }));
 
+// **nodesByRow** `::  Map<edge> ->  Number  -> [Node]`
+// returns an array of nodes  with the specified row id
 export const nodesByRow = grid => (row = 0) =>
   nodes(grid).filter(sameRow({ row }));
 
+// **nodesByPVector** `:: Map<edge> ->  (Number, Number)  -> [Node]`
+// returns an array of nodes on the specified postive diagonal
 export const nodesByPVector = grid => (column = 0, row = 0) =>
   nodes(grid).filter(samePVector({ column, row }));
 
+// **nodesByNVector** `:: Map<edge> ->  (Number, Number)  -> [Node]`
+// returns an array of nodes on the specified negative diagonal
 export const nodesByNVector = grid => (column = 0, row = 0) =>
   nodes(grid).filter(sameNVector({ column, row }));
 
+// **nodeByPosition** `::  Map<edge> ->  node  -> Node`
+// returns a node at the specified position
 export const nodeByPosition = grid => (column = 0, row = 0) =>
   nodes(grid).find(isEquivalent({ column, row }));
--- src/node.js
+++ src/node.js
@@ -1,27 +1,75 @@
 const { atan, abs, PI } = Math;
 
+// **column** `::  Node ->  Number`
+// returns a node's column property
 export const column = ({ column }) => column;
+
+// **row** `::  Node ->  Number`
+// returns a node's row property
 export const row = ({ row }) => row;
+
+// **nodeString** `::  Node ->  String`
+// returns a string representation of a node
 export const nodeString = ({ column, row }) => `{ node::${column}_${row} }`;
+
+// **node** `::  (Number, Number) -> Node`
+// returns an object with column and row properties
 export const node = (column = null, row = null) =>
  ({ column, row, toString: () => nodeString({ column, row }) });
+
+ // **copy** `::  Node -> Node`
+ // returns a copy of a node
 export const copy = n => node(column(n), row(n));
 
+// **colDiff** `::  Node-> Node -> Number`
+// returns the difference of two nodes column properties
 export const colDiff = ({ column: c0 }) => ({ column: c1 }) => (c0 - c1);
+
+// **rowDiff** `::  Node-> Node -> Number`
+// returns the difference of two nodes row properties
 export const rowDiff = ({ row: r0 }) => ({ row: r1 }) => (r0 - r1);
+
+// **tangent** `::  Node-> Node -> Number`
+// returns the column difference to row difference ratio of two nodes
 export const tangent = n0 => n1 => rowDiff(n0)(n1) / colDiff(n0)(n1);
+
+// **angleBetween** `::  Node -> Node -> Number`
+// returns a the angle between two nodes in radians
 export const angleBetween = n0 => n1 => ((atan(tangent(n0)(n1)) % PI) + PI) % PI;
 
+// **sameCol** `:: Node -> Node -> Boolean`
+// checks for equality between two nodes column properties
 export const sameCol = n0 => n1 => abs(colDiff(n0)(n1)) === 0;
+
+// **sameRow** `::  Node -> Node -> Boolean`
+// checks for equality between two nodes row properties
 export const sameRow = n0 => n1 => abs(rowDiff(n0)(n1)) === 0;
+
+// **samePVector** `::  Node -> Node -> Boolean``
+// checks if two nodes lie on the same positive diagonal
 export const samePVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.25;
+
+// **sameNVector** `::  Node -> Node -> Boolean`
+// checks if two nodes lie on the same negative diagonal
 export const sameNVector = n0 => n1 => angleBetween(n0)(n1) === PI * 0.75;
 
+// **cAdj** `::  Node -> Node -> Boolean`
+// checks if two nodes lie on the same column
 export const cAdj = n0 => n1 => abs(colDiff(n0)(n1)) < 2;
+
+// **rAdj** `:: Node -> Node -> Boolean
+// checks if two nodes lie on the same row
 export const rAdj = n0 => n1 => abs(rowDiff(n0)(n1)) < 2;
 
+// **isEquivalent** `::  Node -> Node -> Boolean`
+// checks if two nodes share position
 export const isEquivalent = c0 => c1 => sameCol(c0)(c1) && sameRow(c0)(c1);
+
+// **xEquivalent** `::  Node -> Node -> Boolean`
+// checks if two nodes don't share position
 export const xEquivalent = src => alt => !isEquivalent(src)(alt);
 
+// **isNeighbor** `::  Map<edge> ->  node  -> Map<edge>`
+// checks if two different nodes are neighbors
 export const isNeighbor = n0 => n1 =>
  xEquivalent(n0)(n1) && cAdj(n0)(n1) && rAdj(n0)(n1);
