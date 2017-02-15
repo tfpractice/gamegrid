1b64c6f084556873a8dae889d11e743f87dcfa0a
theory friction practice <jackson.brebnor@gmail.com>
2017-02-15 17:04:18 -0500

    index exports

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import * as Components from './components';
 import * as Grid from './grid';
 import * as Join from './join';
 import * as Node from './node';
+import * as Filter from './filter';
+import * as Compare from './compare';
 
 export { Adj, Components, Grid, Join, Node, };
